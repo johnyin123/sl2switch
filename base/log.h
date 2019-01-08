d515a66daf121cb0df45d1ad5fa620ad0636f646
johnyin <johnyin.news@163.com>
2019-01-08 10:52:42 +0800

    add log timestamp

--- base/log.h
+++ base/log.h
@@ -15,10 +15,18 @@
 
 extern int logfile;
 extern int log_level;
+
+#include <time.h>
 static inline void __log(int priority, const char *fmt, ...)
 {
+    char str[64];
+    time_t now = time(NULL);
     if ((priority > log_level) || (logfile == 0))
         return;
+    if (strftime(str, sizeof(str), "%Y-%m-%d %H:%M:%S", localtime(&now)))
+    {
+        dprintf(logfile, "[%s]", str);
+    }
     va_list ap;
     va_start(ap, fmt);
     vdprintf(logfile, fmt, ap);
